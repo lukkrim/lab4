962a69223ab8d62897a59864fef1a292969e81b4
Samson wan <wanfranek@Samsons-MacBook-Pro.local>
2024-04-23 13:31:27 -0700

    ArrayExamples fixed

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2(){
+    int[] arr = {1,2,3,4};
+    int[] reverse = {4,3,2,1};
+    assertArrayEquals(reverse, ArrayExamples.reversed(arr));
+  }
+
+  @Test
+  public void testReverseInPlace2(){
+    int[] input1 = {1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2,1}, input1);
+  }
 }
